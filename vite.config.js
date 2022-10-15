bd1d6c8fb6992f79753541c5151be72b5b9c9754
duck21v7k003 <duck21v7k003@vlvh.ctu.edu.vn>
2022-10-15 14:34:48 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3001,
-    },    
+    port: 3001, proxy: {
+    "/api": {
+    target: "http://localhost:3000/", changeOrigin: true,
+    },
+    }
+    },       
   plugins: [vue()],
   resolve: {
     alias: {
