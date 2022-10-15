22b2b0d540a0a6887a592df2f048d66ad2a2db3b
duck21v7k003 <duck21v7k003@vlvh.ctu.edu.vn>
2022-10-15 14:55:44 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,12 @@ import { createWebHistory, createRouter } from "vue-router"; import ContactBook 
 const routes = [
 {
     path: "/:pathMatch(.*)*", name: "notfound",
-    component: () => import("@/views/NotFound.vue"),    
-},
+    component: () => import("@/views/NotFound.vue"),
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true // Truyền các biến trong $route.params vào làm props
+    },
 ];
 
 
